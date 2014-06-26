5710f603fba292dd38332e3281e0d4c9d2c001b8
ML_Sports <seidlt@cip.ifi.lmu.de>
2014-06-26 13:52:37 +0200

    changed minor error in plot3,4

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,9 @@
+##Download houseold power consumption data
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="power_data.zip")
+
+data <- read.table(unz("power_data.zip", "household_power_consumption.txt"), header=TRUE, sep=";", colClasses="character", stringsAsFactors=FALSE )
+
+
 ## Convert Columns (1 - Date, 2 - ch, 3 - 9 numeric) and add Column datetime
 data[,1]<- as.Date(data[,1], "%d/%m/%Y")
 data$datetime <- paste(data[,1], data[,2])
